cffb84c908558669aa698ac71ad4072c31198d9e
nisticism <nistacular@gmail.com>
2020-04-24 01:40:40 -0700

    resource creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :games
+  resources :matches
+  resources :tournaments
   resources :players, only: [:index, :show, :new, :create, :update, :delete]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
