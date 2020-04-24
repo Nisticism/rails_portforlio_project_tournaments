25b6880d2b4b502a1cd6683a9f67d871b04cf632
nisticism <nistacular@gmail.com>
2020-04-24 00:35:30 -0700

    create resource players

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  resources :players, only: [:index, :show, :new, :create, :update, :delete]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :recipes, only: [:index, :show, :new, :create]
 end
--- db/migrate/20200424073237_create_players.rb
+++ db/migrate/20200424073237_create_players.rb
@@ -1,6 +1,11 @@
 class CreatePlayers < ActiveRecord::Migration[5.0]
   def change
     create_table :players do |t|
+      t.String :name
+      t.String :email
+      t.String :password
+
+      t.timestamps
     end
   end
 end
